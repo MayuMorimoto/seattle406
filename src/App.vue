9d032253812f0ca14eef04559ce350b86a68a364
Kohei.Yoshida <kohei.yoshida@seattleconsulting.co.jp>
2021-05-17 20:40:23 +0900

    add: map

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <About></About>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import About from '@/components/About.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    About
+  },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// LeafletのCSSを読み込み
+import 'leaflet/dist/leaflet.css';
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
